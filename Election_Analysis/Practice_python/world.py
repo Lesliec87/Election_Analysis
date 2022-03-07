9a9bda34694eaa5925867a7f7db71a307b47bb2d
Lesliec87 <99294497+Lesliec87@users.noreply.github.com>
2022-03-07 17:27:40 -0600

    Add files via upload

---
+++ Election_Analysis/Practice_python/world.py
@@ -0,0 +1,32 @@
+# Create a variable called 'name' that holds a string
+
+name = "class"
+# Create a variable called 'country' that holds a string
+country = "US"
+
+# Create a variable called 'age' that holds an integer
+age = 20
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 100
+
+# Calculate the daily wage for the user
+daily_wage = hourly_wage * 8
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = True
+
+# Print out "Hello <name>!"
+print("Hello " + name + !)
+
+# Print out what country the user entered
+print("country")
+
+# Print out the user's age
+print()
+
+# With an f-string, print out the daily wage that was calculated
+
+
+# With an f-string, print out whether the users were satisfied
+
