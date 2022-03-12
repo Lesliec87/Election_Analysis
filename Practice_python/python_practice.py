1874dd7f19dc2b9d7704d7c42a0b24dd13bbeab2
Your Name <you@example.com>
2022-03-12 11:15:41 -0600

    test

---
+++ Practice_python/python_practice.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+name = "Cardi B"
+words = "Hell Yeah"
+Satisfied = True
+
+print(f"Like {name} says")
+print(words)
+print(f"{Satisfied} Dat")
+
---
+++ Practice_python/world.py
@@ -0,0 +1,33 @@
+# Create a variable called 'name' that holds a string
+
+name = "Leslie"
+# Create a variable called 'country' that holds a string
+country = "US"
+
+# Create a variable called 'age' that holds an integer
+age = 20
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 100
+
+# Calculate the daily wage for the user
+daily_wage = hourly_wage * 8
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = True
+
+# Print out "Hello <name>!"
+print("Hello my name is " + name)
+
+# Print out what country the user entered
+print("country")
+
+# Print out the user's age
+print()
+
+# With an f-string, print out the daily wage that was calculated
+print(f"I make {daily_wage} {country} a day")
+
+# With an f-string, print out whether the users were satisfied
+print(f"This is {satisfied}")
+
